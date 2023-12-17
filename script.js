162fd7694e08f9714b4db74ca105836febb6bafc
Ashutosh Pratap Singh <023.ashutosh@gmail.com>
2023-12-18 00:54:59 +0530

    1.2

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-let len = 
+let len = document.getElementById("len")
+let generate = document.getElementById("generate")
+let view = document.getElementById("view")
 
+const genPassword = (len) => {
 
-let pass ;
+  let pass = ""
 
-const genPassword = ((len) =>{ 
-        
-    let pass = ""
-    
-    let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    let lowerCase = "abcdefghijklmnopqrstuvwxyz"
-    let number = `0123456789`
-    let symbol = `!@#$%^&*()"<>/,.?;'\][]{}~`
-    
-    if(len<4){
-        console.log("Password shoiuld be atleast 4 letters.")
-    }   
-    else{
-        let i=0
-        while(i<len){
-            pass = pass + upperCase [ Math.floor( Math.random() * upperCase.length) ]
-            pass = pass + lowerCase [ Math.floor( Math.random() * lowerCase.length) ]
-            pass = pass + number [ Math.floor( Math.random() * number.length) ]
-            pass = pass + symbol [ Math.floor( Math.random() * symbol.length) ]
-            i+=4;
-        }
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  let number = `0123456789`
+  let symbol = `!@#$%^&*()"<>/,.?;'\][]{}~`
 
-        pass = pass.substr(0,len)
-        return pass
+  if (len < 4) 
+  {
+    console.log("Password shoiuld be atleast 4 letters.")
+  }
+  else 
+  {
+    let i = 0
+    while (i < len) 
+    {
+      pass += upperCase[Math.floor(Math.random() * upperCase.length)]
+      pass += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+      pass += number[Math.floor(Math.random() * number.length)]
+      pass += symbol[Math.floor(Math.random() * symbol.length)]
+      i += 4;
     }
-        
-})
+
+    pass = pass.substr(0, len)
+
+  }
+  view.innerHTML = `<h3> ${pass}</h3>`
+}
+generate.addEventListener("click", () => genPassword(len.value))
+
